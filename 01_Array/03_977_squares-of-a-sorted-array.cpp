995daf1b557ca880a07594aa5a5c97909de43343
unknown <867909454@qq.com>
2023-07-18 20:29:38 +0800

    'update'

---
+++ 01_Array/03_977_squares-of-a-sorted-array.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int abs(int num)
+{
+    return num >= 0 ? num : -num;
+}
+
+vector<int> sortedSquares(vector<int> &nums)
+{
+    int k = nums.size() - 1;
+    vector<int> res(nums.size(), 0);
+    int i = 0, j = nums.size() - 1;
+    while (i <= j)
+    {
+        if (nums[i] * nums[i] < nums[j] * nums[j])
+        {
+            res[k--] = nums[j] * nums[j];
+            --j;
+        }
+        else
+        {
+            res[k--] = nums[i] * nums[i];
+            ++i;
+        }
+    }
+    return res;
+}
+
+int main(int argc, char const *argv[])
+{
+    vector<int> nums{-4, -1, 0, 3, 10};
+    auto res = sortedSquares(nums);
+    for (int i = 0; i < res.size(); ++i)
+    {
+        cout << res[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
